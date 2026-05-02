930553fd05c9f5e069ddf0e11d80aee39bf3309a
Your Name <hardic@axioned.com>
2026-05-02 17:46:26 +0530

    Updated About Us Page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,7 +7,7 @@ export default function About() {
 
   return (
     <div style={{display:"flex", justifyContent:"center", alignItems:"center", height:"100vh"}}>
-      <h1>About Us Page</h1>
+      <h1>About Us Page - Updated</h1>
     </div>
   );
 }
